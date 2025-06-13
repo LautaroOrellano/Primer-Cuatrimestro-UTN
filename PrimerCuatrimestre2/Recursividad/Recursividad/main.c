2c0e6c741e6f31dedf9f2156c33f9b4cb6dd1499
Lautaro Orellano <Lautaa123@gmail.com>
2025-06-13 00:50:00 -0300

    practica de recursividad

--- PrimerCuatrimestre2/Recursividad/Recursividad/main.c
+++ PrimerCuatrimestre2/Recursividad/Recursividad/main.c
@@ -1,149 +1,245 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-//Prototipado
-int recursividadFactorial(int num);
-int recursividadPotencia(int numP, int potencia);
-void mostrarArregloFormaRecursiva(int arreglo[],int i, int validos);
-void mostrarArregloFormaRecursivaAlrevez(int arreglo[],int i, int validos);
-int palabraCapicua(int numero[] , int i, int j);
+//-----------------------Prototipado----------------------
+int factorialNumer(int n0);
+int potenciaNum(int n, int p);
+int recorrerArreglo(int arreglo[], int i, int dimension);
+int recorrerArregloInvertido(int arreglo[], int i, int dimension);
+int arregloCapicua(int arregloCap[], int indice, int dimension1);
+int sumarElementosDeUnArreglo(int arregloParaSumar[], int dimension2, int suma, int indice1);
+int menorElementoDelArreglo(int arregloParaBuscarMenor[], int dimension3, int menorElemento, int indice2);
+void crearArchivo(char archivo[]);
+void leerArchivo(char archivo[]);
+//--------------------------------------------------------
 
 int main()
 {
+
     ///Ejercicio1
+    int n0 = 5;
+    int nro = factorialNumer(n0);
 
-    //int num, rta;
-    //printf("Numero para el factorial: \n");
-    //fflush(stdin);
-    //scanf("%d", &num);
+    printf("Factorial de %d: %d \n", n0, nro);
 
-    //rta = recursividadFactorial(num);
-    //printf("factorial de %d: %d\n", num, rta);
+    system("pause");
+    system("cls");
 
-    ///Ejercicio2
+    ///Ejrcicio2
 
-    //int numP, potencia, rta2;
-    //printf("Numero para potenciar: \n");
-    //fflush(stdin);
-    //scanf("%d", &numP);
+    int p = 2;
+    int n = 5;
+    int nro2 = potenciaNum(n,p);
+    printf("%d a la %d: %d \n", n, p, nro2);
 
-    //printf("\n");
-
-    //printf("Numero de potencia: \n");
-    //fflush(stdin);
-    //scanf("%d", &potencia);
-
-    //rta2 = recursividadPotencia(numP, potencia);
-    //printf("potencia de %d a la %d: %d\n", numP, potencia, rta2);
+    system("pause");
+    system("cls");
 
     ///Ejercicio3
 
-    int arreglo[] = {1,2,3,4};
-    int validos = 4;
+    int arreglo[] = {10,20,30};
     int i = 0;
-    mostrarArregloFormaRecursiva(arreglo, i, validos);
-    printf("\n");
+    int dimension = 3;
+
+    printf("ARREGLO: \n");
+    int retorno = recorrerArreglo(arreglo, i, dimension);
+
+    system("pause");
+    system("cls");
 
     ///Ejercicio4
 
-    mostrarArregloFormaRecursivaAlrevez(arreglo, i, validos);
+    printf("ARREGLO INVERTIDO: \n");
+    int retorno1 = recorrerArregloInvertido(arreglo, i, dimension);
+
+    system("pause");
+    system("cls");
 
     ///Ejercicio5
-    int numero[]= {3, 5, 9, 4, 3};
-    int retorno = 0;
 
-    retorno = palabraCapicua(numero, 0, 4);
+    int arregloCap[] = {1,2,3,2,1};
+    int indice = 0;
+    int dimension1 = 5;
 
-    if(retorno == 0){
-        printf("El numero no es capicua \n");
+    int condicionDeArreglo = arregloCapicua(arregloCap, indice, dimension1);
+
+    if(condicionDeArreglo == 0){
+
+        printf("El arreglo no es capicua \n");
     } else {
-        printf("El numero es capicua \n");
+        printf("El arreglo si era capicua \n");
     }
+
+    system("pause");
+    system("cls");
 
     ///Ejercicio6
 
+    int arregloParaSumar[] = {10, 10, 10, 30, 40};
+    int dimension2 = 5;
+    int indice1 = 0;
+    int suma = 0;
 
+    int sumaTotal = sumarElementosDeUnArreglo(arregloParaSumar, dimension2, suma, indice1);
 
+    printf("La suma total del arreglo es: %d \n", sumaTotal);
 
+    system("pause");
+    system("cls");
+
+    ///Ejercicio7
+
+    int arregloParaBuscarMenor[] = {48,6,3,43,2,45,56};
+    int dimension3 = 7;
+    int indice2 = 0;
+    int menorElemento = arregloParaBuscarMenor[0];
+    int menorEncontrado = menorElementoDelArreglo(arregloParaBuscarMenor, dimension3, menorElemento, indice2);
+
+    printf("El menor elemento del arreglo es: %d \n", menorEncontrado);
+
+    system("pause");
+    system("cls");
+
+    ///Ejercicio8
+    char archivo[] = "archivo.bin";
+
+    //crearArchivo(archivo);
+    printf("Contenido Archivo: \n\n");
+    leerArchivo(archivo);
+
+    system("pause");
+    system("cls");
 
     return 0;
 }
 
-int recursividadFactorial(int num){
+int factorialNumer(int n0){
 
-    int rta;
-
-    if(num == 0){
-
-        rta = 1;
-
-    } else {
-        rta = num * recursividadFactorial(num-1);
-    }
-
-    return rta;
-}
-
-int recursividadPotencia(int numP, int potencia){
-
-    int rta = 0;
-
-    if(potencia == 0){
-
-        rta = 1;
-
-    } else {
-        rta = numP * recursividadPotencia(numP, potencia-1);
-    }
-
-    return rta;
-}
-
-void mostrarArregloFormaRecursiva(int arreglo[], int i, int validos){
-
-    if(i < validos){
-
-        printf("%d", arreglo[i]);
-
-       mostrarArregloFormaRecursiva(arreglo, i+1, validos);
-    }
-}
-
-void mostrarArregloFormaRecursivaAlrevez(int arreglo[], int i, int validos){
-
-
-    if(i < validos){
-
-        mostrarArregloFormaRecursivaAlrevez(arreglo, i+1, validos);
-        printf("%d", arreglo[i]);
-    }
-}
-
-int palabraCapicua(int numero[] ,int i, int j){
-
-    if(i >= j){
+    if(n0 == 0){
 
         return 1;
-    }
-    if (numero[i] != numero[j]){
-
-        return 0;
-    }
-
-    return palabraCapicua(numero, i+1, j-1);
-}
-
-int sumaRecursiva(int numero[], int i, int validos){
-
-    int = rta;
-
-    if(i < validos-1){
-
-        rta = num[i];
 
     } else {
-        rta = numero[i] + sumaRecursiva(numero, i+1, validos);
+
+        return n0 * factorialNumer(n0-1);
+    }
+}
+
+int potenciaNum(int n, int p){
+
+    if(p == 0){
+
+        return 1;
+
+    } else {
+        return n * potenciaNum(n, p-1);
+    }
+}
+
+int recorrerArreglo(int arreglo[], int i, int dimension){
+
+    if(i >= dimension){
+
+        return 1;
+
+    } else {
+        printf("%d ", arreglo[i]);
+        return recorrerArreglo(arreglo, i+1, dimension);
+    }
+}
+
+int recorrerArregloInvertido(int arreglo[], int i, int dimension){
+
+    if(i == dimension){
+
+        return 1;
+
+    } else {
+        printf("%d ", arreglo[dimension-1]);
+        return recorrerArregloInvertido(arreglo, i, dimension-1);
+    }
+}
+
+int arregloCapicua(int arregloCap[], int indice, int dimension1){
+
+    if(indice >= dimension1){
+
+        return 1;
+
     }
 
-   return rta;
+    if(arregloCap[indice] != arregloCap[dimension1-1]) {
+
+        return 0;
+
+    } else
+
+    return arregloCapicua(arregloCap, indice+1, dimension1-1);
+}
+
+int sumarElementosDeUnArreglo(int arregloParaSumar[], int dimension2, int suma, int indice1){
+
+    if(indice1 >= dimension2){
+
+        return 0;
+    } else {
+
+        suma = arregloParaSumar[indice1] + sumarElementosDeUnArreglo(arregloParaSumar, dimension2, suma, indice1+1);
+        return suma;
+    }
+}
+
+int menorElementoDelArreglo(int arregloParaBuscarMenor[], int dimension3, int menorElemento, int indice2){
+
+    if(indice2 >= dimension3){
+
+        return menorElemento;
+    }
+
+    if(menorElemento > arregloParaBuscarMenor[indice2+1]){
+
+        menorElemento = arregloParaBuscarMenor[indice2+1];
+
+        return menorElementoDelArreglo(arregloParaBuscarMenor, dimension3, menorElemento, indice2 + 1);
+
+    } else {
+
+        return menorElementoDelArreglo(arregloParaBuscarMenor, dimension3, menorElemento, indice2 + 1);
+    }
+
+}
+
+void crearArchivo(char archivo[]){
+
+    FILE* arch = fopen(archivo, "wb");
+
+    if(arch != NULL){
+
+        for(int i=0; i<10; i++){
+
+            fwrite(&i, sizeof(int), 1, arch);
+        }
+
+        fclose(arch);
+    } else {
+        printf("Error al abrir el archivo. \n");
+    }
+}
+
+void leerArchivo(char archivo[]){
+
+    FILE* arch = fopen(archivo, "rb");
+    int aux;
+
+    if(arch != NULL){
+
+        while(fread(&aux, sizeof(int), 1, arch) > 0){
+
+            printf("%d ", aux);
+        }
+
+        fclose(arch);
+    } else {
+        printf("Error al abrir el archivo. \n");
+    }
 }
